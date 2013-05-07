f4a59623d4955491d59f29dee29fae72db8a0ce1
bzzazzj <bassam507681@gmail.com>
2013-05-08 01:48:28 +0300

    Added a LED functionality to the arduino code. If cosm responded with error or didn't respond LED blinks otherwise the LED is on

--- src/arduino/SpaceSwitchCosm/SpaceSwitchCosm.ino
+++ src/arduino/SpaceSwitchCosm/SpaceSwitchCosm.ino
@@ -12,6 +12,9 @@ char cosmKey[] = "60quPHrNGggy6peIYZzU12kcE7WSAKwwcTM4R1pCcGF6Zz0g";
 // Analog pin which we're monitoring (0 and 1 are used by the Ethernet shield)
 int sensorPin = 2;
 
+//Digital pin connected to LED
+int ledPin = 1;
+
 // Define the strings for our datastream IDs
 char sensorId[] = "sensor_reading";
 CosmDatastream datastreams[] = {
@@ -25,15 +28,22 @@ CosmClient cosmclient(client);
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT); 
+  digitalWrite(ledPin, LOW); 
   Serial.begin(9600);
   
   Serial.println("Starting single datastream upload to Cosm...");
   Serial.println();
+  
+  // blink once at start up
+  Blink(ledPin, 1000);
+ 
 
   while (Ethernet.begin(mac) != 1)
   {
     Serial.println("Error getting IP address via DHCP, trying again...");
-    delay(15000);
+    Blink(ledPin, 1000);
+    delay(14000);  
   }
 }
 
@@ -48,7 +58,23 @@ void loop() {
   int ret = cosmclient.put(feed, cosmKey);
   Serial.print("cosmclient.put returned ");
   Serial.println(ret);
-
   Serial.println();
-  delay(15000);
+  if(ret<0) 
+  {
+    // If the return is unsuccessful LED blinks 7 times
+    for(int i=0; i<7; i++) Blink(ledPin, 1000); 
+  }else 
+  {
+    // If the return is successful LED is on
+    digitalWrite(ledPin, HIGH);   
+    delay(15000);
+  }
+}
+
+void Blink(int pin, int delay_ms)
+{
+   digitalWrite(pin, HIGH);   // turn the LED on (HIGH is the voltage level)
+   delay(delay_ms);               // wait for a second
+   digitalWrite(pin, LOW);    // turn the LED off by making the voltage LOW
+   delay(delay_ms); 
 }
